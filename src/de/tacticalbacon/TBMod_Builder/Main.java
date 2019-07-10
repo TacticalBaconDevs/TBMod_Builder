e4127efe901e0e6811f468f4ba78441f20169f1a
shukari <shukari@users.noreply.github.com>
2019-07-10 13:04:08 +0200

    1.0.4_dev

--- src/de/tacticalbacon/TBMod_Builder/Main.java
+++ src/de/tacticalbacon/TBMod_Builder/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
@@ -17,14 +18,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
-	private static final String cfgfile = "builder.cfg";
-	private static final String version = "1.0.2";
+	private static final String cfgfile = new File("builder.cfg").getAbsolutePath();
+	private static final String version = "1.0.4";
 	private static Properties properties = new Properties();
 	private static Map<String, String> overrides = new HashMap<>();
 	private static boolean buildFailure = false;
+	private static String arguments = "-A -B -D -P -U -X\\=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar -@\\=x\\\\TBMod\\\\addons\\\\<ADDON_NAME>";
+	private static String reqVersion = "MakePbo x64UnicodeVersion 1.99, Dll 7.16";
 
 	public static void main(String[] args) throws Exception {
 		// per Doppelklick gestartet, neu mit Console aufrufen
@@ -32,11 +37,9 @@ public class Main {
 		boolean inIDE = System.getProperty("java.class.path").toLowerCase().contains("eclipse");
 		if (!inIDE && console == null && !GraphicsEnvironment.isHeadless()) {
 			String filename = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
-			Runtime.getRuntime().exec(String.format("cmd /c start cmd /c java -jar \"%s\"", filename));
+			Runtime.getRuntime().exec(String.format("cmd /c start cmd /c java -jar \"%s\" %s", filename, String.join(" ", args)));
 			return;
 		}
-
-		System.out.println("TBMod Builder v" + version + " gestartet...");
 
 		// Argument-Support
 		if (args.length != 0) {
@@ -45,20 +48,37 @@ public class Main {
 			List<String> argumente = Arrays.asList(args);
 			argumente.stream().map((item) -> item.startsWith("-") ? item.substring(1).trim() : item.trim()).forEach((item) -> {
 				// PropertiesOverride
-				if (item.contains(":")) {
-					String[] split = item.split(":");
-					overrides.put(split[0].trim(), split[1].replace("\"", "").trim());
+				if (item.contains("=")) {
+					String[] split = item.split("=");
+					overrides.put(split[0].trim().substring(1), split[1].replace("\"", "").trim());
+				} else {
+					overrides.put(item.trim(), "");
 				}
 			});
 		}
-
+		
+		System.out.println("TBMod Builder v" + version + " gestartet...");
+		debug("Ausgeführt in: " + new File("addons").getAbsolutePath());
+		
+		System.setProperty("user.dir", overrides.getOrDefault("Duser.dir", new File("").getAbsolutePath()));
+		
+		// MakePBO & DLL (Version)-Check
+		Process process = Runtime.getRuntime().exec("MakePbo.exe -P");
+		BufferedReader inputerror = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+		String output = inputerror.readLine();
+		
+		if (!output.contains(reqVersion))
+			System.out.println("!!!ACHTUNG!!! Die DLL oder MakePBO Version weicht von der für den Builder gemachten Version ab. Derzeit: "+ output +" | Builder: "+ reqVersion);
+		
 		try {
 			initConfig();
+			
+			System.setProperty("user.dir", getProperty("ExecDir"));
 
 			String outputDir = getProperty("OutputDir");
 
-			File[] addons = new File("addons").listFiles(file -> file.getName().startsWith("TBMod"));
-			File[] existingaddons = new File(outputDir).listFiles(file -> file.getName().startsWith("TBMod"));
+			File[] addons = new File(new File("addons").getAbsolutePath()).listFiles(file -> !file.getName().startsWith("#") && file.isDirectory());
+			File[] existingaddons = new File(outputDir).listFiles(file -> file.getName().startsWith("TBMod_") && !file.isDirectory());
 
 			for (File addonfolder : addons)
 				processAddon(addonfolder, outputDir);
@@ -66,7 +86,7 @@ public class Main {
 			for (File existingaddon : existingaddons) {
 				boolean found = false;
 				for (File addon : addons) {
-					if (existingaddon.getName().startsWith(addon.getName())) {
+					if (existingaddon.getName().equals("TBMod_"+ addon.getName())) {
 						found = true;
 						break;
 					}
@@ -86,15 +106,27 @@ public class Main {
 			requestClose();
 		}
 	}
+	
+	@SuppressWarnings("resource")
+	public static String execCmd(String cmd) throws java.io.IOException {
+	    Scanner s = new Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
+	    String result = s.hasNext() ? s.next() : "";
+	    s.close();
+	    return result;
+	}
 
 	private static void requestClose() throws IOException {
 		System.out.println("Drücke Enter zum beenden.");
 		System.in.read();
 	}
-
+	
 	private static void initConfig() throws Exception {
 		if (new File(cfgfile).exists()) {
 			properties.load(new FileInputStream(cfgfile));
+
+			// ### setzt sur Übersicht immer die
+			properties.setProperty("recommendedArgs", arguments);
+			properties.store(new FileOutputStream(cfgfile), "TBMod Builder");
 		} else {
 			System.out.println("Keine Konfiguration gefunden, starte Erstellung!");
 
@@ -147,6 +179,16 @@ public class Main {
 			scanner.close();
 			properties.store(new FileOutputStream(cfgfile), "TBMod Builder");
 		}
+		
+		// ### Configupdate 1.0.4
+		if (!properties.containsKey("currentArgs")) {
+			properties.setProperty("currentArgs", arguments);
+			properties.store(new FileOutputStream(cfgfile), "TBMod Builder");
+		}
+		if (!properties.containsKey("ExecDir")) {
+			properties.setProperty("ExecDir", new File("").getAbsolutePath());
+			properties.store(new FileOutputStream(cfgfile), "TBMod Builder");
+		}
 	}
 
 	private static void processAddon(File addonfolder, String outputDir) throws Exception {
@@ -159,20 +201,44 @@ public class Main {
 				System.out.print("   ");
 			
 			File pbo = new File(String.format("%s\\%s.pbo", outputDir, addonfolder.getName()));
-			if (pbo.exists() && getLastModified(addonfolder).before(getLastModified(pbo))) {
+			if (pbo.exists() && getLastModified(addonfolder).before(getLastModified(pbo))) { // TODO: wenn im neuen eine File gelöscht, dann überspringt er trotzdem
 				System.out.println("\t>>> Überspringe");
 				return;
 			}
-
-			Process p = new ProcessBuilder("MakePbo.exe", "-A", "-B", "-D", "-P", "-U", "-X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar", addonfolder.toString(), outputDir).start();
+			
+			String args = getProperty("currentArgs").replaceAll("<ADDON_NAME>", addonfolder.getName());
+			
+			// Baue Befehl zusammen
+			List<String> command = new ArrayList<>();
+			command.add("MakePbo.exe");
+			command.addAll(Arrays.asList(args.split(" ")));
+			command.add(addonfolder.toString());
+			command.add(outputDir + "\\TBMod_"+ addonfolder.getName());
+			
+			ProcessBuilder processBuilder = new ProcessBuilder(command);
+			Process process = processBuilder.start();
+			debug("\nDebug Buildcmd: "+ String.join(" ", command));
+			
 			String line;
 			boolean error = true;
-			inputerror = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-			input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			inputerror = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			input = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			StringBuilder incaserror = new StringBuilder();
+			
+			// MakePbo ohne etwas läuft unednlich ohne Ausgabe
+			Timer timeout = new Timer();
+			timeout.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					incaserror.append("!!! TIMEOUT !!!\n");
+					process.destroy();
+				}
+			}, 5000);
+			
 			while ((line = input.readLine()) != null) {
 				incaserror.append(line + "\n");
 			}
+			
 			while ((line = inputerror.readLine()) != null) {
 				incaserror.append(line + "\n");
 				if (line.contains("No Error(s)")) {
@@ -180,6 +246,9 @@ public class Main {
 					error = false;
 				}
 			}
+			
+			timeout.cancel();
+			
 			if (error) {
 				buildFailure = true;
 				System.out.println("\t!!! wurde nicht erfolgreich gebaut. Fehler folgt...");
@@ -229,6 +298,11 @@ public class Main {
 
 		Arrays.sort(moddates, (o1, o2) -> o2.compareTo(o1));
 		return new Date(moddates[0]);
+	}
+	
+	private static void debug(String msg) {
+		if (overrides.containsKey("debug"))
+			System.out.println(msg);
 	}
 
 }
